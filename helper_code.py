0d2a3613af0313c2d1ce26c8df4b427e51d5268d
Synthia S <cynthiasynthia2077@gmail.com>
2024-03-29 20:17:24 -0500

    fixed some things

--- helper_code.py
+++ helper_code.py
@@ -4,15 +4,15 @@ import pandas as pd
 
 def readInData(path, type):
     """
+    Reads in the data into a dataframe, this function may be kinda useless but
+    it can clean up the code and make changes a bit easier
+
     path: path to dataset (string)
     type: type of file, such as txt, csv, tsv (string)
 
     returns a dataframe 
     """
 
-    data = pd.DataFrame()
-
-    #Read in the data based on file type
     if type == "txt":
         data = pd.read_table(path)
     elif type == "csv" or type == "tsv":
@@ -20,16 +20,18 @@ def readInData(path, type):
     else:
         print("Unsupported data format")
         return
-    
-    if data == None:
-        print("Something went wrong reading in data")
 
     return data
     
 def getWantedColumns(df, cols):
     """
+    One part of data cleaning process, getting only the necessary columns from the dataset,
+    and returning a copy of the dataframe so the original one stays intact
+    
     df: the dataframe you want to get the columns for (dataframe)
     cols: a list of strings of the columns you want to keep (list: string)
+    
+    returns new dataframe with only the wanted columns
     """
 
     df_copy = df.copy(deep=True)
