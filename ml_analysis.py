f250e2c7e945e9d8a46e510c2d317d3b6db20e9c
Luis de Santos <ldesantos1501@gmail.com>
2024-04-03 22:33:38 -0500

    Implemented random forest in NSDUH2018 dataset, plus a cleaning function for type objects in a dataframe

--- ml_analysis.py
+++ ml_analysis.py
@@ -5,6 +5,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 import numpy as np
 
@@ -23,6 +24,9 @@ import numpy as np
 # WEB1j Use of TikTok 1 Yes, 2 No, 3 Dont Know, 4 Refused
 # Inc - Income ranges
 def predictUsageOfAgeGroups(data: pd.DataFrame):
+    """
+        
+    """
     data['age'] = pd.cut(data['age'], bins=[0, 24, 34, 44, 54, 64, float('inf')],
                        labels=['18-24', '25-34', '35-44', '45-54', '55-64', '65+'])
     selected = ['intfreq', 'web1a', 'web1b', 'web1c', 'web1d', 'web1e', 'web1f', 'web1g',
@@ -66,21 +70,57 @@ def feature_selection(X: pd.DataFrame, y: pd.Series, n_features=None):
     print("Feature ranking:")
     print(feature_ranking)
 
-def to_numeric(data: pd.DataFrame, col) -> pd.DataFrame:
-    # Need all ints not objects
+def convertObjects(data: pd.DataFrame) -> pd.DataFrame:
+    """
+        This will grab columns in the data frame that are of type object
+        and convert them to integer, most likely floats which is still fine
+    """
     copy = data.copy()
-    copy[col] = pd.to_numeric(copy[col], errors='coerce', downcast='integer')
-    copy = copy.dropna(subset=[col])
+    columns = copy.select_dtypes(include=['object'])
+    for column in columns.columns:
+        copy[column] = pd.to_numeric(copy[column], errors='coerce', downcast='integer')
+    copy.fillna(-1, inplace=True)
     return copy
 
-nsduh2018 = pd.read_csv('datasets/January 3-10, 2018 - Core Trends Survey/January 3-10, 2018 - Core Trends Survey - CSV.csv')
-nsduh2019 = pd.read_csv('datasets/January-8-February-7-2019-Core-Trends-Survey-SPSS/January 8-February 7, 2019 - Core Trends Survey - CSV.csv')
-nsduh2021 = pd.read_csv('datasets/Jan-25-Feb-8-2021-Core-Trends-Survey/Jan 25-Feb 8, 2021 - Core Trends Survey - CSV.csv')
+def randomForest(data: pd.DataFrame, target):
+    """
+    X: entire dataframe
+    y: target series, i.e what you want to train
+    target: name of target column
 
+    """
+    y = data[target]
+    X = data.drop([target], axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+    clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+
+    print(f'Accuracy: {accuracy * 100:.2f}%')
+
+    cm = confusion_matrix(y_test, y_pred)
+    print(f'Confusion Matrix:\n{cm}')
+
+    cr = classification_report(y_test, y_pred)
+    print(f'Classification report:\n{cr}')
+
+
+# Pretty much playground code, ignore for now
 if __name__ == '__main__':
-    merged = pd.concat([nsduh2018, nsduh2019, nsduh2021], axis=0)
-    cleaned_data = to_numeric(merged, 'intfreq')
-    merged.info()
-    cleaned_data.value_counts()
+    drop_columns = ['usr', 'pial5b', 'pial5c', 'pial5d', 'pial11a', 'pial11ao@', 'pial12']
+    nsduh2018 = pd.read_csv('datasets/January 3-10, 2018 - Core Trends Survey/January 3-10, 2018 - Core Trends Survey - CSV.csv')
+    nsduh2019 = pd.read_csv('datasets/January-8-February-7-2019-Core-Trends-Survey-SPSS/January 8-February 7, 2019 - Core Trends Survey - CSV.csv')
+    nsduh2021 = pd.read_csv('datasets/Jan-25-Feb-8-2021-Core-Trends-Survey/Jan 25-Feb 8, 2021 - Core Trends Survey - CSV.csv')
+    # nsduh2019.info()
+    # nsduh2021.info()
+    # merged = pd.concat([nsduh2018, nsduh2019, nsduh2021], axis=0)
+    # print('====================================================================')
+    # merged.info()
+    # cleaned_data = to_numeric(merged, 'intfreq')
+    # merged.info()
+    # cleaned_data.value_counts()
+    nsduh2018 = nsduh2018.drop(drop_columns, axis=1)
+    nsduh2018 = convertObjects(nsduh2018)
     nsduh2018.info()
-    feature_selection(nsduh2018, pd.Series(nsduh2018['intfreq']))
+    randomForest(nsduh2018, 'intfreq')
