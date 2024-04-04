4aeeca09af4371d1b46fae3e6fdd3842e992fac0
Synthia S <cynthiasynthia2077@gmail.com>
2024-04-04 18:43:29 -0500

    merged readIn and getWantedCols functions

--- helper_code.py
+++ helper_code.py
@@ -1,43 +1,27 @@
 #This python file Is used to store helper functions such as the ones 
 #for loading in and cleaning up data
 import pandas as pd
-
-def readInData(path, type):
-    """
-    Reads in the data into a dataframe, this function may be kinda useless but
-    it can clean up the code and make changes a bit easier
-
-    path: path to dataset (string)
-    type: type of file, such as txt, csv, tsv (string)
-
-    returns a dataframe 
-    """
-
-    if type == "txt" or type == "tsv":
-        data = pd.read_table(path)
-    elif type == "csv":
-        data = pd.read_csv(path)
-    else:
-        print("Unsupported data format")
-        return
-
-    return data
     
-def getWantedColumns(df, cols):
+def readInAndGetWantedColumns(path, type, cols):
     """
     One part of data cleaning process, getting only the necessary columns from the dataset,
     and returning a copy of the dataframe so the original one stays intact
 
+    path: path to dataset as string
+    type: type of file, such as txt, csv, tsv (string)
     df: the dataframe you want to get the columns for (dataframe)
     cols: a list of strings of the columns you want to keep (list: string)
     
     returns new dataframe with only the wanted columns
     """
+    if type == "txt" or type == "tsv":
+        df = pd.read_table(path,low_memory=False)
+    elif type == "csv":
+        df = pd.read_csv(path,low_memory=False)
 
-    df_copy = df.copy(deep=True)
-    df_copy = df_copy[cols]
+    df = df[cols]
 
-    return df_copy
+    return df
 
 def cleanUpCoreTrends(df, id, values):
     """
